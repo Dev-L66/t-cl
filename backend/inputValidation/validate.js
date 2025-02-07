a6b9c72590b74be5c6d3aaee46dc894722bb26b2
Dev-L66 <sabeebashil@yahoo.com>
2025-02-07 17:10:16 +0300

    user routes

--- backend/inputValidation/validate.js
+++ backend/inputValidation/validate.js
@@ -4,7 +4,7 @@ export const validateSignupInput = z.object({
     username: z.string().min(3, { message: "Username must be at least 3 characters" }).max(50),
     fullName: z.string().min(3, { message: "Fullname must be at least 3 characters" }).max(50),
     email: z.string().email().min(3, { message: "email must be at least 3 characters" }).max(30),
-    password: z.string().min(6, { message: "Password must be at least 3 characters" }).max(20)
+    password: z.string().min(6, { message: "Password must be at least 6 characters" }).max(20)
 }).strict();
 
 
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,17 @@
 import express from 'express';
 import authRoutes from './routes/auth.routes.js';
+import userRoutes from './routes/user.routes.js';
 import dotenv from 'dotenv';
 import connectToDb from './config/db.config.js';
 import cookieParser from 'cookie-parser';
+import { v2 as cloudinary } from 'cloudinary';
 
 dotenv.config();
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key:process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLOUDINARY_API_SECRET,
+});
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -14,8 +21,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
+app.use('/api/user', userRoutes);
 
 app.listen(port, () => {
-    console.log(`Server is listentiong to port ${port}`);
+    console.log(`Server is listening to port ${port}`);
     connectToDb();
 })
