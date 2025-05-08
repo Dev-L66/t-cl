37a0ec524c794a452d79f4ca6f3d168c2739f9e5
Dev-L66 <sabeebashil@yahoo.com>
2025-05-08 13:19:34 +0300

    notification controllers

--- backend/routes/notification.route.js
+++ backend/routes/notification.route.js
@@ -1,5 +1,5 @@
 import { protectedRoute } from "../middleware/protectedRoute.js";
-import {getNotifications, deleteNotifications} from '../controllers/notification.controller.js'
+import {getNotifications, deleteNotifications, deleteNotification} from '../controllers/notification.controller.js'
 import express from 'express';
 
 
@@ -7,5 +7,6 @@ const router = express.Router();
 
 router.get("/", protectedRoute, getNotifications);
 router.delete("/", protectedRoute, deleteNotifications);
+router.delete("/:id", protectedRoute, deleteNotification);
 
 export default router;
