09113b905a44c0d73b33bc7295557d73732aa1ff
Dev-L66 <sabeebashil@yahoo.com>
2025-05-25 23:18:07 +0300

    ..

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
+import path from 'path';
 import express from 'express';
-import authRoutes from '../routes/auth.route.js';
-import userRoutes from '../routes/user.route.js';
-import postRoutes from '../routes/post.route.js';
-import notificationRoutes from '../routes/notification.route.js';
+import authRoutes from './routes/auth.route.js';
+import userRoutes from './routes/user.route.js';
+import postRoutes from './routes/post.route.js';
+import notificationRoutes from './routes/notification.route.js';
 import dotenv from 'dotenv';
-import connectToDb from '../config/db.config.js';
+import connectToDb from './config/db.config.js';
 import cookieParser from 'cookie-parser';
 import { v2 as cloudinary } from 'cloudinary';
 import rateLimit from 'express-rate-limit';
@@ -18,10 +19,10 @@ cloudinary.config({
 
 const app = express();
 const port = process.env.PORT || 4000;
-
 app.use(express.json({ limit: '5mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
 
 let limiter = rateLimit({
     max: 500,
@@ -33,6 +34,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/posts', postRoutes);
 app.use('/api/notifications', notificationRoutes);
+
 
 app.listen(port, () => {
     console.log(`Server is listening to port ${port}`);
