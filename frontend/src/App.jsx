6b3fc946cdce0621d0794692d379ecd51b9e304c
Dev-L66 <sabeebashil@yahoo.com>
2025-05-27 01:42:14 +0300

    done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ import ProfilePage from "./pages/profile/ProfilePage";
 import { Toaster } from "react-hot-toast";
 import { useQuery } from "@tanstack/react-query";
 import LoadingSpinner from "./components/common/LoadingSpinner";
-import { API } from "./utils";
+import { API } from "./utils/index.js";
 
 const App = () => {
   const {data:authUser, isLoading, isError, error} = useQuery({
