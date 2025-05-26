752d5ecc6cf705d36392a4c5c0e7a76081e551af
Dev-L66 <sabeebashil@yahoo.com>
2025-05-26 23:48:58 +0300

    done

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,10 @@ cloudinary.config({
 
 const app = express();
 const port = process.env.PORT || 4000;
+app.cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+});
 app.use(express.json({ limit: '5mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
