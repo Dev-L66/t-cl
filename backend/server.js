2cfc1756447ee38407f3c3d86aac25cf4eb9b823
Dev-L66 <sabeebashil@yahoo.com>
2025-05-28 10:01:44 +0300

    ..

--- backend/server.js
+++ backend/server.js
@@ -24,8 +24,6 @@ app.use(
   cors({
     origin: [process.env.CLIENT_URL, "http://localhost:3000"],  
     credentials: true, 
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    optionsSuccessStatus: 200,
   })
 );
 app.use(express.json({ limit: '5mb' }));
