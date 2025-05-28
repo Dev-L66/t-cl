4bb582adc4148c65b0a00b42b7b60cbb34083704
Dev-L66 <sabeebashil@yahoo.com>
2025-05-28 09:13:20 +0300

    ..

--- backend/lib/utils/generateTokenAndSetCookie.js
+++ backend/lib/utils/generateTokenAndSetCookie.js
@@ -2,11 +2,14 @@ import jwt from 'jsonwebtoken';
 
 export const generateTokenAndSetCookie = (userId, res) => {
     const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: '3d' });
+    res.json({ token });
+
+    const isProductiion = process.NODE_ENV === 'production';
     
     res.cookie('token', token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV !== 'development',
+        secure: isProduction,
         maxAge: 3 * 24 * 60 * 60 * 1000, 
-        sameSite: "None",
+        sameSite: isProduction ? 'none' : 'lax',
         });
 }
--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,9 @@ app.use(
   cors({
     origin: [process.env.CLIENT_URL, "http://localhost:3000"],  
     credentials: true, 
-        
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'COOKIE'],
+    optionsSuccessStatus: 200
   })
 );
 app.use(express.json({ limit: '5mb' }));
