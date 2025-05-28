1c67aa19c8487965292d701ad3eb6eae04dc39ed
Dev-L66 <sabeebashil@yahoo.com>
2025-05-28 09:56:24 +0300

    .

--- backend/lib/utils/generateTokenAndSetCookie.js
+++ backend/lib/utils/generateTokenAndSetCookie.js
@@ -2,14 +2,13 @@ import jwt from 'jsonwebtoken';
 
 export const generateTokenAndSetCookie = (userId, res) => {
     const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: '3d' });
-    
+    res.json({ token });
 
-    const isProduction = process.NODE_ENV === 'production';
     
     res.cookie('token', token, {
         httpOnly: true,
-        secure: isProduction,
+        secure: process.env.NODE_ENV !== 'development',
         maxAge: 3 * 24 * 60 * 60 * 1000, 
-        sameSite: isProduction ? 'none' : 'lax',
+        sameSite: "None",
         });
 }
