1c836e8de0124c0dac3bceb1ffcc06bcbc9305ba
Dev-L66 <sabeebashil@yahoo.com>
2025-05-28 09:22:09 +0300

    ..

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use(
     origin: [process.env.CLIENT_URL, "http://localhost:3000"],  
     credentials: true, 
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'COOKIE'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
     optionsSuccessStatus: 200
   })
 );
