dd28080d82c0b2b5ed0d6c7181d74fa817ac2383
Dev-L66 <sabeebashil@yahoo.com>
2025-05-29 16:09:02 +0300

    ..

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,12 +8,12 @@ export default defineConfig({
   server:{
     host: true,
     port: 3000,
-    // proxy: {
-    //   "/api":{
-    //     target: "http://localhost:5000",
-    //     changeOrigin: true
-    //   }
-    // }
-
+    proxy: {
+      '/api': {
+        target: 'http://localhost:4000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   }
 })
