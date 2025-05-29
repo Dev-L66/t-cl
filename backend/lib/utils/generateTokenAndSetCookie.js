29a3da708822e4a5bf2481200c173d58ed96f5c9
Dev-L66 <sabeebashil@yahoo.com>
2025-05-30 00:02:20 +0300

    ..

--- backend/lib/utils/generateTokenAndSetCookie.js
+++ backend/lib/utils/generateTokenAndSetCookie.js
@@ -5,7 +5,7 @@ export const generateTokenAndSetCookie = (userId, res) => {
   
     res.cookie('token', token, {
         httpOnly: true,
-        secure: true,
+        secure: process.env.NODE_ENV === 'production',
         maxAge: 3 * 24 * 60 * 60 * 1000, 
         sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
         });
