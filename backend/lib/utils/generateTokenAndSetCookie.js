24c04a1f17fc7bc4413ba2f7a7dd4107f4f77060
Dev-L66 <sabeebashil@yahoo.com>
2025-05-29 22:03:44 +0300

    ..

--- backend/lib/utils/generateTokenAndSetCookie.js
+++ backend/lib/utils/generateTokenAndSetCookie.js
@@ -7,6 +7,6 @@ export const generateTokenAndSetCookie = (userId, res) => {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
         maxAge: 3 * 24 * 60 * 60 * 1000, 
-        same_site: process.env.NODE_ENV === "production" ? "none" : "lax",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
         });
 }
